50fde773dd8725305b5f957bccdae7adb4e4bc83
blackreo123 <blackreo123@naver.com>
2021-07-05 15:08:08 +0900

    slider added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import './App.css';
 import MyCard from './components/MyCard/MyCard';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import styled from 'styled-components';
+import shoe1 from './images/shoe1.jpeg';
+import shoe2 from './images/shoe2.jpeg';
+import shoe3 from './images/shoe3.jpg';
+import shoe4 from './images/shoe4.webp';
+import shoe5 from './images/shoe5.jpg';
+import shoe6 from './images/shoe6.webp';
 function App() {
+
+  
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
     <div className="grid-container">
       <div className="my-top">
@@ -13,12 +32,29 @@ function App() {
       </div>
       <div className="my-side">side</div>
       <div className="my-contents">
-        <MyCard></MyCard>
-        <MyCard></MyCard>
-        <MyCard></MyCard>
-        <MyCard></MyCard>
-        <MyCard></MyCard>
-        <MyCard></MyCard>
+      <div className="my-slider">
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <img src={shoe1} />
+          </div>
+          <div>
+          <img src={shoe2} />
+          </div>
+          <div>
+          <img src={shoe3} />
+          </div>
+          <div>
+          <img src={shoe4} />
+          </div>
+          <div>
+          <img src={shoe5} />
+          </div>
+          <div>
+          <img src={shoe6} />
+          </div>
+        </Slider>
+      </div>
       </div>
       <div className="my-foot">foot</div>
     </div>
